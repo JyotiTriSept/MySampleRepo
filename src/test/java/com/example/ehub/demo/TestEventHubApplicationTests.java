0a9b7123628fa3e861eaca04afdec2418cd284f1
JyotiKumari <jyotikumari.jyotikumari@harman.com>
2023-03-09 16:57:36 +0530

    initail commit

---
+++ src/test/java/com/example/ehub/demo/TestEventHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ehub.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEventHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
